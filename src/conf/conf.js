1b2d231d4331d5fd024b622c25e23b997f5f0689
sujan khatri <sujansince2059@gmail.com>
2024-02-19 23:04:38 +0545

    ENV

---
+++ src/conf/conf.js
@@ -0,0 +1,9 @@
+const conf = {
+  appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+  projectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+  collectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+  bucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+  databaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+};
+
+export default conf;
