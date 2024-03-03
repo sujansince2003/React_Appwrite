1e8543b863ed42d5a8b6467feb3f177f5c59cad7
sujan khatri <sujansince2059@gmail.com>
2024-03-03 19:56:22 +0545

    component index update added react hook form used authservice in app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,41 @@
+import { useEffect, useState } from "react";
+import authservice from "./appwrite/auth";
+
 import "./App.css";
+import { useDispatch } from "react-redux";
+import { login, logout } from "./store/authSlice";
+import { Header } from "./Components";
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITEURL);
+  const [isLoading, setisLoading] = useState(true);
+  const dispatch = useDispatch();
 
-  return <>hello</>;
+  // useEffect
+
+  useEffect(() => {
+    authservice
+      .getCurrentUser()
+      .then((userData: any) => {
+        console.log(userData);
+        if (userData) dispatch(login({ userData }));
+        else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => {
+        setisLoading(false);
+      });
+  }, []);
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+        {isLoading ? "loading" : "data has arrived"}
+        Hello world!
+      </h1>
+      <Header />
+    </>
+  );
 }
 
 export default App;
---
+++ src/Components/index.tsx
@@ -0,0 +1,9 @@
+import Footer from "./footer/Footer";
+import Header from "./header/Header";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import Logoutbtn from "./header/Logoutbtn";
+import Input from "./Input";
+import Button from "./Button";
+
+export { Header, Footer, Container, Logo, Logoutbtn, Input, Button };
