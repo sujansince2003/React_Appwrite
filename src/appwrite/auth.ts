689be27ca26788b219824552523532ae33e4328e
sujan khatri <sujansince2059@gmail.com>
2024-04-26 08:49:31 +0545

    appwrite auth service :: added comments for better understanding

--- src/appwrite/auth.ts
+++ src/appwrite/auth.ts
@@ -9,6 +9,7 @@ interface createAccountParams {
   name: string;
 }
 
+
 interface loginParams
 {
   email: string;
@@ -17,12 +18,19 @@ interface loginParams
 }
 
 export class Authservice {
-  client = new Client();
+  client = new Client();   
   account;
+  /**
+   we can initialize client outside or inside constructor. Both approaches  are valid, and the choice between them depends on your specific needs and preferences. Initializing client outside the constructor ensures that it is available as soon as the instance is created, which can be useful if you need to use client in the constructor or if you want to ensure that client is initialized before any other code runs.
+
+On the other hand, initializing client inside the constructor can make the code more organized and clear, especially if the initialization logic is closely tied to the constructor's responsibilities. However, it also means that client is not available until after the constructor has been called, which could be a consideration depending on how your class is used.
+   */
 
   constructor() {
-    this.client.setEndpoint(conf.appwriteUrl).setProject(conf.projectId);
-    this.account = new Account(this.client);
+    this.client.setEndpoint(conf.appwriteUrl).setProject(conf.projectId); //configure instance of Client with url and project id
+    
+    this.account = new Account(this.client);  //passing client instance to Account 
+   
   }
 
   async createAccount({ email, password, name }:createAccountParams):Promise<any> {
@@ -53,13 +61,15 @@ export class Authservice {
     }
   }
 
+
+  //this method is used to check if user is logged in or not.also can be used to get details about curent loggedin user
   async getCurrentUser():Promise<any>  {
     try {
       return await this.account.get();
     } catch (error) {
       throw error;
     }
-    return null;
+    return null;     //null is written explictily so that if user is not logged in then it should return null
   }
 
   async logout():Promise<void> {
@@ -72,4 +82,4 @@ export class Authservice {
 }
 const authservice = new Authservice(); //object
 
-export default authservice; //exporting object
+export default authservice; //exporting object    this will contains all methods defined under Authservice class
