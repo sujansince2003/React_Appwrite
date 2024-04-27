13515269742b02b79ef60861c4b36cbc5bf91046
sujan khatri <sujansince2059@gmail.com>
2024-04-27 23:26:40 +0545

    added realtimeeditor pkg

--- src/Components/index.tsx
+++ src/Components/index.tsx
@@ -5,5 +5,17 @@ import Logo from "./Logo";
 import Logoutbtn from "./header/Logoutbtn";
 import Input from "./Input";
 import Button from "./Button";
+import RealTimeEditor from "./RealTimeEditor";
+import Select from "./Select";
 
-export { Header, Footer, Container, Logo, Logoutbtn, Input, Button };
+export {
+  Header,
+  Footer,
+  Container,
+  Logo,
+  Logoutbtn,
+  Input,
+  Button,
+  Select,
+  RealTimeEditor,
+};
