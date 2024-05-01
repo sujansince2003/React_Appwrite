02b6bef95d18c82727c783247fadc62508cfff44
sujan khatri <sujansince2059@gmail.com>
2024-05-01 21:02:51 +0545

    import and export components

---
+++ src/Components/index.ts
@@ -0,0 +1,31 @@
+import Footer from "./footer/Footer";
+import Header from "./header/Header";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import Logoutbtn from "./header/Logoutbtn";
+import Input from "./Input";
+import Button from "./Button";
+import RealTimeEditor from "./RealTimeEditor";
+import Select from "./Select";
+import SignUp from "./Signup";
+import Login from "./Login";
+import PostCard from "./PostCard";
+import Postform from "./postForm/Postform";
+import Authlayout from "./Authlayout";
+
+export {
+  Header,
+  Footer,
+  Container,
+  Logo,
+  Logoutbtn,
+  Input,
+  Button,
+  Select,
+  RealTimeEditor,
+  SignUp,
+  Login,
+  PostCard,
+  Postform,
+  Authlayout,
+};
---
+++ src/Components/pages/index.ts
@@ -0,0 +1,9 @@
+import Home from "./Home";
+import AllpostsPage from "./AllpostsPage";
+import LoginPage from "./LoginPage";
+import AddPost from "./AddPost";
+import EditPost from "./EditPost";
+import Post from "./Post";
+import SignupPage from "./SignupPage";
+
+export { Home, AllpostsPage, LoginPage, AddPost, EditPost, Post, SignupPage };
