e203ba37c86b0c6510ecef027c7ef9d694067f27
sujan khatri <sujansince2059@gmail.com>
2024-05-01 16:34:46 +0545

    component  to edit posts

---
+++ src/Components/pages/EditPost.tsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import { Container, Postform } from "../index";
+import service from "../../appwrite/config";
+import { useNavigate, useParams } from "react-router-dom";
+
+function EditPost() {
+  const [post, setPost] = useState<any>();
+  const { slug } = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (slug) {
+      service.getPost(slug).then((post: any) => {
+        if (post) setPost(post);
+      });
+    } else {
+      navigate("/");
+    }
+  }, []);
+  return post ? (
+    <div className="py-8">
+      <Container>
+        <Postform post={post} />
+      </Container>
+    </div>
+  ) : null;
+}
+
+export default EditPost;
