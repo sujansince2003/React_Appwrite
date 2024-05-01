eb1220cd350b4ac9ebcb8d9370e7692b9dd222f8
sujan khatri <sujansince2059@gmail.com>
2024-05-01 16:36:49 +0545

    signup component also added html react parser to convert html to reac tcomponents

---
+++ src/Components/pages/SignupPage.tsx
@@ -0,0 +1,11 @@
+import { SignUp as Signupcomponent } from "../index";
+
+function SignupPage() {
+  return (
+    <div className="py-8">
+      <Signupcomponent />
+    </div>
+  );
+}
+
+export default SignupPage;
