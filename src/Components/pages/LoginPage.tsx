c7688ba6689debf6ddf1ac1200e3390d8338a8f9
sujan khatri <sujansince2059@gmail.com>
2024-05-01 16:35:18 +0545

    Login component

---
+++ src/Components/pages/LoginPage.tsx
@@ -0,0 +1,11 @@
+import { Login as Logincomponent } from "../index";
+
+function LoginPage() {
+  return (
+    <div className="py-8">
+      <Logincomponent />
+    </div>
+  );
+}
+
+export default LoginPage;
