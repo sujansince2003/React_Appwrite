7e217a587a351eca78741bf2a0c60ff0bcc2ec35
sujan khatri <sujansince2059@gmail.com>
2024-05-05 08:50:07 +0545

    Added: Postslice for posts state management and reduce API requests to server

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./authSlice";
+import postReducer from "./postSlice"
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
+    reduxposts:postReducer,
   },
 });
 
