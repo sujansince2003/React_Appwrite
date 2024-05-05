e2ddf79b94c7333eba5dcdc831d445f873e95cbe
sujan khatri <sujansince2059@gmail.com>
2024-05-05 20:47:37 +0545

    modified posts page for fetching data

--- src/Components/pages/AllpostsPage.tsx
+++ src/Components/pages/AllpostsPage.tsx
@@ -1,27 +1,27 @@
-// import { useEffect } from "react";
-// import service from "../../appwrite/config";
+import { useEffect } from "react";
+import service from "../../appwrite/config";
 
 import { PostCard, Container } from "../index";
-import { useSelector } from "react-redux";
-// import { setreduxPosts } from "../../store/postSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { setreduxPosts } from "../../store/postSlice";
 
 function AllpostsPage() {
-  // const dispatch = useDispatch();
+  const dispatch = useDispatch();
   // using redux store
 
   const { reduxposts } = useSelector((state: any) => state.reduxposts);
 
   // we dont need this because we are using redux store for managing state of posts
 
-  // useEffect(() => {
-  //   //   getting all posts  ::listdocuments from appwrite comes in array
-  //   service.getAllPost([]).then((posts) => {
-  //     if (posts) {
-  //       // Dispatch the transformed posts to the Redux store
-  //       dispatch(setreduxPosts(posts.documents));
-  //     }
-  //   });
-  // }, [dispatch]);
+  useEffect(() => {
+    //   getting all posts  ::listdocuments from appwrite comes in array
+    service.getAllPost([]).then((posts) => {
+      if (posts) {
+        // Dispatch the transformed posts to the Redux store
+        dispatch(setreduxPosts(posts.documents));
+      }
+    });
+  }, [dispatch]);
 
   return (
     <div className="py-8">
